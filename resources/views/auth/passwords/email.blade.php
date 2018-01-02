e5637e7acc426a42fddc0179630fe30551b41158
Edwin Ajahuanca Callisaya <edwinajahuancacallisaya@gmail.com>
2018-01-02 06:26:34 -0400

    email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,43 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>SISEMO</title>
+    <link rel="stylesheet prefetch" href="{{ asset('plugins/login/css/font.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/login/css/style.css') }}">
+</head>
+<body>
+<div class="cont">
+    <div class="demo">
+        <div class="login">
+            <div class="login__check">
+                <img src="{{ asset('plugins/login/img/logo.jpg') }}" width="273px" height="80px" />
+                <p class="titulo">RECUPERAR CONTRASEÑA</p>
             </div>
+            <form method="POST" action="{{ route('password.email') }}">
+                <div class="login__form">
+                    {{ csrf_field() }}
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                    <div class="login__row">
+                        <svg class="login__icon email svg-icon" viewBox="0 0 20 20">
+                            <path d="M0,20 20,20 20,8 0,8z M10,13 10,16z M4,8 a6,8 0 0,1 12,0" />
+                        </svg>
+                        <input type="email" class="login__input email" placeholder="example@example.com" name="email" required="true"/>
+                    </div>
+                    <button type="button" class="login__submit"><b>Enviar enlace</b></button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
-@endsection
+<script src="{{ asset('plugins/login/js/jquery.min.js') }}"></script>
+<script src="{{ asset('plugins/login/js/index.js') }}"></script>
+</body>
+</html>
